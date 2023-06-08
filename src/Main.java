c3ca262ca0015c329595d5437cab5aee171c3086
zhevrakov.ra <zhevrakov.ra.ki25@mail.ru>
2023-06-08 10:35:33 +0300

    jj

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
+
+
+
     public static void main(String[] args) {
-      
+        List<List<Integer>> arr = new ArrayList<>();
+
+        for (int i = 0; i < 10; i++) {
+            List<Integer> stroke = new ArrayList<>();
+
+            for (int k = 0; k < 10; k++) {
+                int randomNumber = (int) (Math.random() * 2);
+
+                stroke.add(randomNumber);
+            }
+            arr.add(stroke);
+        }
+
+        for (List<Integer> stroke : arr) {
+            System.out.println(stroke);
+        }
     }
-}
+    }
